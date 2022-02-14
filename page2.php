6d42917c9039c6fa11b97189c9b60748bde8b7e9
yamxty <98384633+yamxty@users.noreply.github.com>
2022-02-14 17:19:22 +0700

    Delete page2.php

--- page2.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head>
-    <title>เว็บไซต์ของ  พัชรพร บัวเกตุ</title>
-  </head>
-  <body>
-    <a href="https://facebook.com"><h1>Facebook</h1></a>
-    <a href="mypage2.php"><h1>Mypage2</h1></a>
-  </body>
-</html>
