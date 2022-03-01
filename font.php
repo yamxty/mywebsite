825c839768837d2fb4eaf9db5bad5bcce2ea55b3
yamxty <98384633+yamxty@users.noreply.github.com>
2022-03-01 18:28:52 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Itim&family=Mali:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Itim&family=Mali:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Sriracha&display=swap');
+      body {
+        font-size: 1.25rem;
+        text-align: center;
+      }
+      .charm {
+        font-family: 'Charm', cursive;
+      }
+      .pattaya {
+        font-family: 'Pattaya', sans-serif;
+      }
+    </style>
+  </head>
+ <body>
+    <h1 class="charm">พัชรพร บัวเกตุ เลขที่ 21 ห้อง ม.4/12 เลขประจำตัว 57047</h1>
+    <h1 class="pattaya">พัชรพร บัวเกตุ เลขที่ 21 ห้อง ม.4/12 เลขประจำตัว 57047</h1>
+  </body>
+</html>
