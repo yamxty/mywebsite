4f9cb4061a79954ca6eefb144194a7bda6319c07
yamxty <98384633+yamxty@users.noreply.github.com>
2022-03-08 00:24:36 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>lee hi</td>
+        <td><img src="https://i0.wp.com/www.korseries.com/wp-content/uploads/2019/12/lee-hi.jpg?fit=1000%2C596&ssl=1" width="45px"></td>
+        <td><a href="https://www.instagram.com/leehi_hi/">ig</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>lisa</td>
+        <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4QkKum2GvpbAuGu3EqGMD8y9I0LD9t4egccfnCw7qhSMVjKnbwKlglTUsfXEqIs86a4I&usqp=CAU" width="45px"></td>
+        <td><a href="https://www.instagram.com/lalalalisa_m/">ig</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>taehyung</td>
+        <td><img src="https://0.soompi.io/wp-content/uploads/2021/12/21181235/V-5.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/bts_twt">twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Taylor Swift</td>
+        <td><img src="https://scontent.fbkk5-7.fna.fbcdn.net/v/t1.6435-9/149316533_10158572378855369_9048515504720973180_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeF447wXd6qgOw__4ufz3neAo8woeg4hd8SjzCh6DiF3xI3T9_aNVj134ZAqGeM6cVVeNecnwZdds6ejZoXnqAZl&_nc_ohc=A-AqXi39StMAX__t5QK&_nc_ht=scontent.fbkk5-7.fna&oh=00_AT_mEjL-OdPe4MRNB6klFznoT6WyNaaz9b7LuU3jrvScEA&oe=624503B0" width="45px"></td>
+        <td><a href="https://www.facebook.com/TaylorSwift">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>niki</td>
+        <td><img src="https://www.naewna.com/uploads/news/source/415616.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/nikizefanya/">ig</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
